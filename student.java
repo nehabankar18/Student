a1862d078491e6508fc93649979addb9501cf2ea
nehabankar18 <156497515+nehabankar18@users.noreply.github.com>
2024-02-19 14:11:11 +0530

    Update student.java

--- student.java
+++ student.java
@@ -2,7 +2,7 @@
 public class student {
     public void stud_det()
     {
-        String name = "Prajwal";
+        String name = "Neha";
         String prg = "BCA";
         String dept = "MCA";
         String[] course = {"DevOps", "Machine Learning", "C#"};
